61b3340bf4acd7cbd90a3fbac06043c3798b687a
Carmen Salinas <31458839+carmensalinas@users.noreply.github.com>
2024-06-16 21:28:06 +0000

    Copilot third commit2

--- comments.js
+++ comments.js
@@ -1,29 +1,18 @@
-// Create a web server
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const fs = require('fs');
+// Create a web server that listens on port 8080
+// When you navigate to http://localhost:8080/comments, it should return a list of comments in JSON format
+// The comments should be stored in a file called comments.json
+// The file should contain an array of objects
 
-app.use(bodyParser.json());
+// Load the http module to create an http server.
+var http = require('http');
+var fs = require('fs');
 
-// Create a comments.json file
-const comments = require('./comments.json');
-fs.writeFileSync('./comments.json', JSON.stringify(comments), 'utf8');
-
-// Get all comments
-app.get('/comments', (req, res) => {
-  res.json(comments);
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  fs.readFile('comments.json', function(err, data) {
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    response.end(data);
+  });
 });
 
-// Add a comment
-app.post('/comments', (req, res) => {
-  const comment = req.body;
-  comments.push(comment);
-  fs.writeFileSync('./comments.json', JSON.stringify(comments), 'utf8');
-  res.json(comment);
-});
-
-// Listen to port
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
-});
+// Listen on port 8080, IP defaults to
